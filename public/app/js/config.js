ffb319752dea326d36a81bd3a8385ace25f0762b
Alex <project.demonio@gmail.com>
2014-11-12 21:46:09 -0500

    Fixing the layout

--- public/app/js/config.js
+++ public/app/js/config.js
@@ -12,7 +12,6 @@ var config_module = angular.module('myAppConfig', [])
         'WEEKEND_DAYS': [5, 6, 0],
         'WEEK_DAYS': [1, 2, 3, 4, 5, 6, 0],
         'DEFAULT_CATEGORIES': {
-            "happenson": "today"
         },
         'CATEGORY_SELECTOR': 'views/components/categories-selector.html'
 
