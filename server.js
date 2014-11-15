3fad35aa1f5de042c41cb5628ae03974a34deb32
Alex <project.demonio@gmail.com>
2014-11-15 18:13:15 -0500

    Changing the port to Heroku specs

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var events = require('./routes/events.js');
 var categories = require('./routes/categories.js');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000))
+
+app.use(express.static(__dirname + '/public/app'));
 app.get('/api/events', events.findAll);
 app.get('/api/categories', categories.findAll);
 
@@ -15,6 +17,6 @@ process.on('uncaughtException', function (error) {
    console.log(error.stack);
 });
 
-
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
