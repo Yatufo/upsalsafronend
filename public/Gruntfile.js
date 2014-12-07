2b53aee0c5b41198bdfa1183354a9797b83a09e1
Alex <project.demonio@gmail.com>
2014-12-06 19:14:12 -0500

    Working on the main image and adding the grunt livereload using the grunt watch command

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,23 +1,35 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'app/js/**/*.js',
-        dest: 'app/js/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'app/js/**/*.js',
+                dest: 'app/js/<%= pkg.name %>.min.js'
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: 'Gruntfile.js'
+            },
+            html: {
+                files: ['app/*.html', 'app/assets/css/*.css'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
 };
