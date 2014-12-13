cf01aa7f1c70acdd20c56947045f839425bca6cb
Alex <project.demonio@gmail.com>
2014-12-13 13:32:43 -0500

    Adding to title case to the title

--- public/app/js/controllers/homeCtrl.js
+++ public/app/js/controllers/homeCtrl.js
@@ -6,9 +6,18 @@ angular.module('myAppControllers')
     .controller('HomeCtrl', ['$scope', '$rootScope', '$routeParams', '$http', 'CONFIG',
         function($scope, $rootScope, $routeParams, $http, CONFIG) {
             $rootScope.CONFIG = CONFIG;
-            
+
             $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-                $scope.city = current.params.city;
+                $scope.city = toTitleCase(current.params.city);
             });
+
+
+            var toTitleCase = function(str) {
+                if (str) {
+                    return str.replace(/\w\S*/g, function(txt) {
+                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                    });
+                }
+            }
         }
     ]);
