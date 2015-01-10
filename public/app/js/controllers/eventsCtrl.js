88f5d90e53cdaa77f9a8e0db551d7d13e560e635
Alex <project.demonio@gmail.com>
2015-01-10 16:09:30 -0500

    Creating the app types in a different file

--- public/app/js/controllers/eventsCtrl.js
+++ public/app/js/controllers/eventsCtrl.js
@@ -52,27 +52,6 @@ angular.module('myAppControllers')
 
 
             MapsService.init();
-
-            function Set() {
-                this.content = {};
-            }
-            Set.prototype.content = function() {
-                return this.content;
-            }
-            Set.prototype.add = function(val) {
-                this.content[val] = true;
-            }
-            Set.prototype.remove = function(val) {
-                delete this.content[val];
-            }
-            Set.prototype.contains = function(val) {
-                return (val in this.content);
-            }
-            Set.prototype.asArray = function() {
-                var res = [];
-                for (var val in this.content) res.push(val);
-                return res;
-            }
         }
 
 
