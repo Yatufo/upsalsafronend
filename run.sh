153864c59b251e8e50d566a9d848433c9bc401f9
Alex <project.demonio@gmail.com>
2015-07-01 20:45:15 -0400

    Adding a run script for the server

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+start() {
+    nodemon server.js &
+    grunt watch &
+    mongod --dbpath=data/db
+}
+
+stop() {
+    pkill -f "nodemon server.js"
+    pkill -f "grunt watch"
+    pkill -f "mongod --dbpath=data/db"
+}
+
+case "$1" in
+    "")      start ;;
+    start)   start ;;
+    stop)    stop ;;
+    restart) stop; start ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
