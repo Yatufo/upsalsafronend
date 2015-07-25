fe34eb03b216a47cc594873d91e9c5fe003264cf
Alex <project.demonio@gmail.com>
2015-07-25 11:15:10 -0400

    Refactor token config

--- public/app/js/config/TokenValidationConfig.js
+++ public/app/js/config/TokenValidationConfig.js
@@ -1,19 +1,3 @@
-'use strict';
-
-var eventify = angular.module('eventify');
-
-
-
-var TokenConfig = function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
-  // We're annotating this function so that the `store` is injected correctly when this file is minified
-  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
-    // Return the saved token
-    return store.get('token');
-  }];
-
-  $httpProvider.interceptors.push('jwtInterceptor');
-}
-
 
 eventify.config(["authProvider", "$routeProvider", "$httpProvider", "jwtInterceptorProvider", TokenConfig]);
 
