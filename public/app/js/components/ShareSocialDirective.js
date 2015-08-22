e6b1ed5f4979ff26dfee8ac876b3e58cd0e1018b
Alex <project.demonio@gmail.com>
2015-08-21 22:37:08 -0400

    Improving the coed

--- public/app/js/components/ShareSocialDirective.js
+++ public/app/js/components/ShareSocialDirective.js
@@ -1,5 +1,3 @@
-
-
 /* App Module */
 
 angular.module('eventify').directive('sharesocial', ["$window", "AnalyticsService", function($window, analyticsService) {
@@ -10,12 +8,16 @@ angular.module('eventify').directive('sharesocial', ["$window", "AnalyticsServic
       url: '='
     },
     controller: ['$scope', function($scope) {
-      $scope.facebookUrl = "https://www.facebook.com/sharer/sharer.php?&u=" + encodeURIComponent($scope.url);
-      $scope.twitterUrl = "https://twitter.com/intent/tweet?text=" + $scope.message + "&url=" + encodeURIComponent($scope.url);
-      $scope.whatsappUrl = "whatsapp://send?text=" + $scope.message + " " + $scope.url;
 
-      $scope.share = function(shareUrl) {
-        $window.open(shareUrl, 'sharesocial', 'height=450, width=550, top=' +
+      $scope.urls = {
+        'facebook': "https://www.facebook.com/sharer/sharer.php?&u=" + encodeURIComponent($scope.url),
+        'twitter': "https://twitter.com/intent/tweet?text=" + $scope.message + "&url=" + encodeURIComponent($scope.url),
+        'whatsapp': "whatsapp://send?text=" + $scope.message + " " + $scope.url
+      };
+
+
+      $scope.share = function(channel) {
+        $window.open($scope.urls[channel], 'sharesocial', 'height=450, width=550, top=' +
           ($window.innerHeight / 2 - 275) + ', left=' + ($window.innerWidth / 2 - 225) +
           ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
 
