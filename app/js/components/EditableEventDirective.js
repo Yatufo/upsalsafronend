db874f2d3d0dc7cf1c19443f3246fa2e2d8d3fb1
Alex <project.demonio@gmail.com>
2016-01-24 16:32:50 -0500

    Fixing bug on date generation

--- app/js/components/EditableEventDirective.js
+++ app/js/components/EditableEventDirective.js
@@ -44,6 +44,7 @@ var EditableEventCardController = function($scope, $rootScope, service, category
       }));
       var rule = new RRule({
         freq: RRule.WEEKLY,
+        wkst: RRule.SU,
         count: $scope.selections.count,
         byweekday: selectedDows,
         dtstart: $scope.event.start.dateTime
