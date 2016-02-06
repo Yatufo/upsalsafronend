b19d9fc09b0a385318cb2c15ab89454dfaeaa8aa
Alex <project.demonio@gmail.com>
2016-02-06 10:08:41 -0500

    Starting testing and editing location in progress

---
+++ app/js/controllers/LocationsEditController.js
@@ -0,0 +1,30 @@
+
+
+/* Controllers */
+
+eventify
+    .controller('LocationsEditController', ['$scope', '$routeParams', 'Location', 'MapsService', 'UtilService', '$location',
+        function($scope, $routeParams, Location, maps, util, $location) {
+
+          Location.get({
+            id: $routeParams.locationId
+          }, function(location) {
+
+            maps.init(location, 14);
+            maps.addLocation(location);
+            location.description = location.description || ""
+            location.detailsUrl = util.getDetailsUrl(location, "location");
+            $scope.location = location;
+
+          });
+
+          $scope.$on('locationSaved', function(e, location) {
+            $location.path(util.getDetailsPath($scope.location, "location"))
+          });
+
+          $scope.$on('locationCancelled', function() {
+            $location.path(util.getDetailsPath($scope.location, "location"))
+          });
+
+        }
+    ]);
