6807cd88a9bc4b47b8db495eef05bd3489e5e1a9
Alex <project.demonio@gmail.com>
2016-03-10 13:16:54 -0500

    Upgrading docker compose

--- init.sh
+++ init.sh
@@ -3,9 +3,7 @@
 export AWS_ACCESS_KEY_ID=AKIAIMKUJAVKMMFQDQYA
 export AWS_SECRET_ACCESS_KEY=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
 export FRONTEND_SERVER=192.168.2.11:5000
-export BACKEND_NETWORK=salsanet
-export MONGO_CONNECTION=mongodb://localhost:27017
+export MONGO_CONNECTION=mongodb://mongo:27017
 export S3_BUCKET=salsa.local
-docker network create --driver bridge $BACKEND_NETWORK
 
 docker-compose up -d & grunt
